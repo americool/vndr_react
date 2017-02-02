41506b3862e2021f280b856b469b86cf6ad03be6
Americool <aberanderson@gmail.com>
2017-02-02 14:43:11 -0500

    maybe almost working

--- src/MapView.js
+++ src/MapView.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {GoogleApiWrapper} from 'google-maps-react';
+import Map from './Map';
 
-const MapView = () => {
-  return (
-    <div>
-      <p> This is the Map View Page </p>
-    </div>
-  );
+export class MapView extends Component {
+  render() {
+    const style = {
+      width: '100vw',
+      height: '100vh'
+    }
+
+    return (
+      <div style={style}>
+        <Map google={this.props.google} />
+      </div>
+    );
+  }
 }
 
-export default MapView;
+export default GoogleApiWrapper({
+  apiKey: process.env.GOOGLE
+})(MapView)
