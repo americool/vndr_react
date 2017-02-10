96201917423736f54bb59b53e3fd4d0ab234f04c
americool <aberanderson@gmail.com>
2017-02-10 02:49:18 -0500

    Mapview (#3)
    * maybe almost working
    
    * some logs
    
    * fixes key (#2)
    
    * map working

--- src/MapView.js
+++ src/MapView.js
@@ -1,11 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Map, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
+import Rebase from 're-base';
+import Marker from './Marker';
+import config from './lib/config';
+import { sortMapData } from './helpers/pickMapData';
 
-const MapView = () => {
-  return (
-    <div>
-      <p> This is the Map View Page </p>
-    </div>
-  );
+const base = Rebase.createClass(config);
+const style = {
+  width: '100vw',
+  height: '100vh'
 }
 
-export default MapView;
+export class MapView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+      isLoading: false,
+      data: [],
+    }
+
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData() {
+    this.setState({ isLoading: true });
+    base.fetch('vendors', {
+      context: this,
+      asArray: true,
+    }).then((data) => {
+      const usableData = sortMapData(data);
+      this.setState({
+        data: usableData,
+        isLoading: false,
+      });
+    }).catch(() => {
+      this.setState({ isLoading: false });
+    });
+  }
+
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    })
+  }
+
+  onMapClicked(props) {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <br/>
+        <Map
+          google={this.props.google}
+          style={style}
+          initialCenter={{
+            lat: 40.454785,
+            lng: -80.021807
+          }}
+          zoom={15}
+          onClick={this.onMapClicked}
+        >
+        {this.state.data.map(vendor => (
+            <Marker
+              name={vendor.vndrName}
+              description={vendor.description}
+              position={{lat: vendor.latitude, lng: vendor.longitude}}
+              onClick={this.onMarkerClick}
+            />
+        ))}
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+            <div>
+              <h2>{this.state.selectedPlace.name}</h2>
+              <p>{this.state.selectedPlace.description}</p>
+            </div>
+          </InfoWindow>
+
+        </Map>
+        <br/>
+      </div>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: process.env.GOOGLE_MAP_KEY
+})(MapView)
